733145a434041b2ebd10751fa629cc0af8af1251
hyunsooseol <snow@cau.ac.kr>
2020-06-18 12:00:23 +0900

    change module name

---
+++ R/dichotomous.b.R
@@ -0,0 +1,142 @@
+# This file is a generated template, your changes will not be overwritten
+
+
+#' Tetrachoric Analysis
+#'
+#' @importFrom R6 R6Class
+#' @import jmvcore
+#' @importFrom magrittr "%>%"
+#' @import psych
+#' @import qgraph
+#' @export
+
+# This file is a generated template, your changes will not be overwritten
+
+dichotomousClass <- if (requireNamespace('jmvcore')) R6::R6Class(
+    "dichotomousClass",
+    inherit = dichotomousBase,
+    private = list(
+
+ #==========================================================
+      .init = function() {
+        # get variables
+        
+        matrix <- self$results$get('matrix')
+        vars <- self$options$get('vars')
+        nVars <- length(vars)
+        
+        
+        # add columns--------
+        
+        for (i in seq_along(vars)) {
+          var <- vars[[i]]
+          
+          matrix$addColumn(
+            name = paste0(var, '[r]'),
+            title = var,
+            type = 'number',
+            format = 'zto'
+          )
+          
+        }
+        
+        # empty cells above and put "-" in the main diagonal
+        
+        for (i in seq_along(vars)) {
+          var <- vars[[i]]
+          
+          values <- list()
+          
+          for (j in seq(i, nVars)) {
+            v <- vars[[j]]
+            
+            values[[paste0(v, '[r]')]]  <- ''
+            
+          }
+          
+         matrix$setRow(rowKey = var, values)
+          
+        }
+        
+        if (length(self$options$vars) <= 1)
+          self$setStatus('complete')
+      },
+      
+      
+      
+#==========================================================
+      .run = function() {
+        # `self$data` contains the data
+        # `self$options` contains the options
+        # `self$results` contains the results object (to populate)
+        
+        
+        self$results$instructions$setContent(
+          "<html>
+            <head>
+            </head>
+            <body>
+            <div class='instructions'>
+            <p>Welcome to Tetrachoric Correlation for doing factor analysis as an input data.</p>
+            
+            <p>To get started:</p>
+            
+            <p>- The input dataset require dichotomous data with the type of numeric-continuous in jamovi.</p>
+            <p>- Just highlight the variables and click the arrow to move it across into the 'Variables' box.</p>
+            
+            <p>If you encounter any errors, or have questions, please e-mail me: snow@cau.ac.kr</a></p>
+            </div>
+            </body>
+            </html>"
+        )
+        
+        
+        # get variables---------------------------------
+        
+        matrix <- self$results$get('matrix')
+        vars <- self$options$get('vars')
+        nVars <- length(vars)
+        
+        mydata <- self$data
+        
+        # compute tetrachoric correlation with psych package--------
+        
+        tetrarho <- psych::tetrachoric(mydata)$rho
+        
+        
+        # populate result----------------------------------------
+        
+        for (i in 2:nVars) {
+          for (j in seq_len(i - 1)) {
+            values <- list()
+            
+            values[[paste0(vars[[j]], '[r]')]] <- tetrarho[i, j]
+            
+            matrix$setRow(rowNo = i, values)
+          }
+        }
+        
+        # Prepare Data For Plot -------
+        image <- self$results$plot
+        image$setState(tetrarho)
+        
+      },
+      
+#================================================================
+      
+      .plot = function(image, ...) {
+        ggm <- self$options$ggm
+        
+        if (!ggm)
+          return()
+        
+        
+        tetrarho <- image$state
+        
+        plot <- qgraph(tetrarho, layout = "spring", details = TRUE)
+        
+        print(plot)
+        TRUE
+        
+      }
+    ))
---
+++ R/partial.b.R
@@ -0,0 +1,207 @@
+
+
+# This file is a generated template, your changes will not be overwritten
+
+
+#' Correlation Analysis
+#'
+#' @importFrom R6 R6Class
+#' @import jmvcore
+#' @importFrom magrittr "%>%"
+#' @import qgraph
+#' @import psych
+#' @export
+
+# `self$data` contains the data
+# `self$options` contains the options
+# ..vars - variables to calculate correlations for
+# ..ctrlvars - variables to control for
+# ..shwSig  - show significance level for correlations (p-values)
+# ..flgSig  - flag significant correlations
+# ..sidSig  - one- or two-tailed significance calculations
+# `self$results` contains the results object (to populate)
+
+partialClass <- if (requireNamespace('jmvcore'))
+  R6::R6Class(
+    "partialClass",
+    inherit = partialBase,
+    private = list(
+
+#==================================================================================
+.init = function() {
+  # get variables--------------------------------------
+  
+  matrix <- self$results$get('matrix')
+  var <- self$options$get('vars')
+  varCtl <- self$options$get('ctrlvars')
+  
+  
+  # whether the procedure is controlling for variables or not-----------
+  
+  matrix$setTitle(ifelse(
+    length(varCtl) > 0,
+    'Partial Correlation Matrix',
+    'Correlation Matrix'
+  ))
+  
+  # Add Columns----------------------------------
+  
+  for (i in seq_along(var)) {
+    matrix$addColumn(
+      name = paste0(var[[i]], '[r]'),
+      title = var[[i]],
+      type = 'number',
+      format = 'zto'
+    )
+    matrix$addColumn(
+      name = paste0(var[[i]], '[rp]'),
+      title = var[[i]],
+      type = 'number',
+      format = 'zto,pvalue',
+      visible = '(shwSig)'
+    )
+  }
+  
+  # Empty cells above and put "-" in the main diagonal-------------------
+  
+  for (i in seq_along(var)) {
+    values <- list()
+    
+    for (j in seq(i, length(var))) {
+      values[[paste0(var[[j]], '[r]')]]  <- ''
+      values[[paste0(var[[j]], '[rp]')]] <- ''
+    }
+    values[[paste0(var[[i]], '[r]')]]  <- '\u2014'
+    values[[paste0(var[[i]], '[rp]')]] <- '\u2014'
+    matrix$setRow(rowKey = var[[i]], values)
+  }
+  
+  # initialize setNote-------------------------------------------------
+  
+  matrix$setNote('sigNte', paste0(
+    ifelse(
+      self$options$get('sidSig') == 'onetailed',
+      'One-tailed significance',
+      'Two-tailed significance'
+    ),
+    ifelse(
+      self$options$get('flgSig'),
+      ': * p < .05, ** p < .01, *** p < .001',
+      ''
+    )
+  ))
+  if (length(self$options$vars) <= 1)
+    self$setStatus('complete')
+},
+
+#====================================================================
+      
+.run = function() {
+
+    self$results$instructions$setContent(
+                "<html>
+            <head>
+            </head>
+            <body>
+            <div class='instructions'>
+            <p>Welcome to partial correlation. </p>
+            <p>To get started:</p>
+            
+            <p>- The input dataset require the measure type of numeric-continuous in jamovi.
+            <p>- Just highlight the variables and click the arrow to move it across into the 'Variables' box.</p> 
+            <p>- If you don't move the variables into 'Controlling for' box, The result table shows Pearson Correlation Matrix.</p>
+            
+            <p> Note: When One variable is dichotomous, the other is continuous, The result table is equivalent to a Point-Biserial Correlation.</P>
+            
+            <p>If you encounter any errors, or have questions, please e-mail me: snow@cau.ac.kr</a></p>
+            
+            </div>
+            </body>
+            </html>")
+
+
+# get variables--------------------------------------------------
+           
+  matrix <- self$results$get('matrix')
+  var <- self$options$get('vars')
+  
+  nVar <- length(var)
+  varCtl <- self$options$get('ctrlvars')
+  nCtl   <- length(varCtl)
+  
+  
+  if (nVar > 1) {
+    m  <-
+      as.matrix(cor(self$data[, c(var, varCtl)], use = 'pairwise', method = 'pearson'))
+    X  <- m[var, var]
+    
+    if (nCtl > 0) {
+      Y  <- m[var, varCtl]
+      pi <- solve(m[varCtl, varCtl])
+      Rp <- cov2cor(X - Y %*% pi %*% t(Y))
+    } else {
+      Rp <- X
+    }
+    
+    df <- dim(self$data)[1] - nCtl
+    Rt <- (Rp * sqrt(df - 2)) / sqrt(1 - Rp ^ 2)
+    if (self$options$sidSig == 'onetailed') {
+      nt = 1
+    } else {
+      nt = 2
+    }
+    Pp <- -nt *  expm1(pt(abs(Rt), (df - 2), log.p = TRUE))
+    
+   # populate results------------------------------------------------
+  
+                for (i in 2:nVar) {
+                    for (j in seq_len(i - 1)) {
+                        values <- list()
+                        values[[paste0(var[[j]], '[r]')]]  <- Rp[i, j]
+                        values[[paste0(var[[j]], '[rp]')]] <- Pp[i, j]
+                        matrix$setRow(rowNo=i, values)
+                        if (self$options$get('flgSig')) {
+                            if      (Pp[i, j] < .001)
+                                matrix$addSymbol(rowNo=i, paste0(var[[j]], '[r]'), '***')
+                            else if (Pp[i, j] < .01)
+                                matrix$addSymbol(rowNo=i, paste0(var[[j]], '[r]'), '**')
+                            else if (Pp[i, j] < .05)
+                                matrix$addSymbol(rowNo=i, paste0(var[[j]], '[r]'), '*')
+                        }
+
+                    }
+                }
+            
+            }
+    
+  #Gaussian Graphical Models with partial correlation----------------
+  
+  mydata<- self$data
+  partial<- psych::partial.r(mydata)
+  
+   # Prepare Data For Plot -------
+     image <- self$results$plot
+     image$setState(partial) 
+     },
+
+#================================================================
+
+.plot = function(image, ...) {
+  ggm <- self$options$ggm
+  
+  if (!ggm)
+    return()
+  
+  
+  partial <- image$state
+  
+  plot <- qgraph(partial, layout = "spring", details = TRUE)
+  
+  print(plot)
+  TRUE
+}
+    ))
+
+   
+     
+  
