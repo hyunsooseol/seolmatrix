95edb1e88909cef83f6b7cf81d0a073a63932530
hyunsooseol <snow@cau.ac.kr>
2024-01-14 18:05:15 +0900

    commit G theory

--- R/00jmv.R
+++ R/00jmv.R
@@ -7,7 +7,7 @@
         `author`="Seol, H.", 
         `year`=2024, 
         `title`="seolmatrix: Correlations suite for jamovi", 
-        `publisher`="(Version 3.8.4) [jamovi module]. URL https://github.com/hyunsooseol/seolmatrix"), 
+        `publisher`="(Version 3.8.5) [jamovi module]. URL https://github.com/hyunsooseol/seolmatrix"), 
     `psych`=list(
         `type`="software", 
         `author`="Revelle,W", 
---
+++ R/gtheory.b.R
@@ -0,0 +1,380 @@
+
+# This file is a generated template, your changes will not be overwritten
+#' @importFrom R6 R6Class
+#' @import jmvcore
+#' @importFrom jmvcore toB64
+#' @importFrom gtheory gstudy
+#' @importFrom gtheory dstudy
+#' @import ggplot2
+#' @export
+
+
+gtheoryClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "gtheoryClass",
+    inherit = gtheoryBase,
+    private = list(
+  
+      .init = function() {
+ 
+        if (is.null(self$data) | is.null(self$options$facet)) {
+          self$results$instructions$setVisible(visible = TRUE)
+          
+        }
+        
+        self$results$instructions$setContent(
+          "<html>
+             <head>
+             </head>
+             <body>
+             <div class='instructions'>
+             <p>___________________________________________________________________________________
+             <p> Feature requests and bug reports can be made on my <a href='https://github.com/hyunsooseol/seolmatrix/issues'  target = '_blank'>GitHub</a>.</p>
+             <p>___________________________________________________________________________________
+             </div>
+             </body>
+             </html>"
+        )
+        
+        
+      },
+      
+      
+      .run = function() {
+        
+        # g theory with R-----------
+        
+        # library(gtheory)
+        # data(Brennan.3.2)
+        # data <- Brennan.3.2
+        # 
+        # formula <- "Score ~ (1 | Person) + (1 | Task) + 
+        # (1 | Rater:Task) +
+        # (1 | Person:Task)"
+        # gstudy.out <- gtheory::gstudy(data = data, 
+        #                               colname.objects = "Person",
+        #                               formula = formula)
+        # coef<- dstudy(gstudy.out, colname.objects = "Person", 
+        #               data = data, colname.scores = "Score")
+        # 
+        # data(Rajaratnam.2)
+        
+        if (is.null(self$options$dep) ||
+            is.null(self$options$id) ||
+            is.null(self$options$facet)) return()
+        
+        
+        dep <- self$options$dep
+        id <- self$options$id
+        sub <- self$options$sub
+        facets <- self$options$facet
+        
+        data <- self$data
+        data <- na.omit(data)
+        data <- as.data.frame(data)
+        
+        ###### Generalizability theory--------------------
+        
+          # facet <- self$options$facet
+          
+          # Formula Example----------------------
+          # formula <- "value ~ (1 | subject) +(1 | rater) + (1 | task) + 
+          # (1 | subject:rater) +
+          # (1 | rater:task) + 
+          # (1 | subject:task)"
+          
+          # Example---------- 
+          # vars <- c('A', 'B', 'C')  # you'll populate this from self$options$...
+          # response <- 'bruce'
+          # fmla <- as.formula(paste0(jmvcore::composeTerm(response), '~', paste(jmvcore::composeTerms(vars), collapse='*')))
+          # trms <- attr(terms(fmla), 'term.labels')
+          # trms[1:6] #example---
+          # funnyTerms <- paste0('(1|', trms, ')')
+          # finalFmla <- paste0(jmvcore::composeTerm(response), '~', paste(funnyTerms, collapse='+'))
+          # finalFmla
+          # 
+          # 
+          # vars <- c(self$options$id, self$options$facet)  
+          # response <- self$options$dep
+          # fmla <- as.formula(paste0(jmvcore::composeTerm(response), '~', paste(jmvcore::composeTerms(vars), collapse='*')))
+          # trms <- attr(terms(fmla), 'term.labels')
+          # funnyTerms <- paste0('(1|', trms, ')')
+          # formula <- paste0(jmvcore::composeTerm(response), '~', paste(funnyTerms, collapse='+'))
+          # 
+          # 
+          # self$results$text1$setContent(formula)
+          # 
+          
+        #------------------------------------------------
+          
+        if(self$options$t=='uni'){
+        
+          formula <- self$options$formula
+          
+          gstudy.out<- gtheory::gstudy(data = data, 
+                                       formula = formula)
+                                    
+          ds<- gtheory::dstudy(gstudy.out, 
+                               colname.objects = id,
+                               data = data, 
+                               colname.scores = dep)
+        
+        # Univariate analysis----------
+          
+        # G study table----------------
+          
+          table<- self$results$g
+          
+          gstudy<- as.data.frame(gstudy.out)
+          
+          var<- as.vector(gstudy[[2]])
+          percent<- as.vector(gstudy[[3]])
+          n <- as.vector(gstudy[[4]])
+          
+          items <- as.vector(gstudy[[1]])
+          
+          for (i in seq_along(items)) {
+            
+            row <- list()
+            
+            row[["var"]] <-var[i]
+            row[["percent"]] <- percent[i]
+            row[["n"]] <- n[i]
+            
+            table$addRow(rowKey = items[i], values = row)
+          }
+          
+          # G study table(Variance components)----------------
+          
+          table<- self$results$d
+          
+          dstudy<- as.data.frame(ds$components)
+          
+          var<- as.vector(dstudy[[2]])
+          percent<- as.vector(dstudy[[3]])
+          n <- as.vector(dstudy[[4]])
+          
+          items <- as.vector(dstudy[[1]])
+          
+          for (i in seq_along(items)) {
+            
+            row <- list()
+            
+            row[["var"]] <-var[i]
+            row[["percent"]] <- percent[i]
+            row[["n"]] <- n[i]
+            
+            table$addRow(rowKey = items[i], values = row)
+          }
+          
+          # self$results$text2$setContent(dstudy.out$generalizability)
+          
+          # Measures of D study---------------
+          gen <- ds$generalizability
+          depe <- ds$dependability
+          uni <- ds$var.universe
+          rel <- ds$var.error.rel
+          abs <- ds$var.error.abs
+          
+          
+          if(isTRUE(self$options$mea)){
+            
+            table<- self$results$mea
+            
+            row <- list()
+            
+            row[['generalizability']] <- gen
+            row[['dependability']] <- depe
+            row[['universe']] <- uni
+            
+            table$setRow(rowNo = 1, values = row)
+            
+          }
+          
+          if(isTRUE(self$options$error)){
+            
+            table<- self$results$error
+            
+            row <- list()
+            
+            row[['relative']] <- rel
+            row[['absolute']] <- abs
+            
+            table$setRow(rowNo = 1, values = row)
+            
+          }
+}
+              
+          if(self$options$t=='mul'){
+            
+            #-------------------------------------------------------------
+            formula1 <- self$options$formula1
+            
+            g1<- gtheory::gstudy(data = data, formula = formula1,
+                                colname.strata = self$options$sub, 
+                                colname.objects = self$options$id)  
+            
+            ds1<- gtheory::dstudy(g1,
+                                 colname.objects = id,
+                                 colname.strata = sub,
+                                 data = data,
+                                 colname.scores = dep)
+            #-----------------------------------------------------
+            self$results$text$setContent(ds1) 
+
+            
+         if(isTRUE(self$options$item)){
+            
+            ng <- self$options$ng
+          
+            res <- list() 
+            
+            for (i in  seq_along(1:ng)){
+              
+              res.df <- lapply(g$within[[as.character(i)]], as.data.frame)
+              
+              res[[i]] <- res.df
+              
+            }
+            
+            tab <- NULL
+            
+            for(i in seq_along(1:ng)){
+              
+              re<- as.data.frame.matrix(res[[i]][['components']])
+              
+              tab[[i]] <- re
+              
+            }
+            
+            tab <- tab
+          # self$results$text$setContent(tab) 
+#--------------------------------------------------------------         
+             tables <- self$results$item
+
+             for(i in seq_along(1:ng)){
+
+               item <- tab[[i]]
+               table <- tables[[i]]
+              
+               names<- row.names(item)
+               dims <- colnames(item)
+
+               for (dim in dims) {
+                 table$addColumn(name = paste0(dim),
+                                 type = 'text',
+                                 combineBelow=TRUE)
+               }
+
+               for (name in names) {
+                 row <- list()
+                 for(j in seq_along(dims)){
+                   row[[dims[j]]] <- item[name,j]
+                 }
+                 table$addRow(rowKey=name, values=row)
+               }
+
+             }
+
+}
+           # Within variance components table------
+           
+           #  table <- self$results$within
+           #  
+           # for(i in seq_along(1:ng)){
+           # 
+           #    table <- self$results$within[[i]]
+           # 
+           #   
+           #    names <- dimnames(tab[[i]])[[1]]
+           #    dims <- dimnames(tab[[i]])[[2]]
+           #    
+           #    for (name in names) {
+           #      row <- list()
+           #      
+           #      for(j in seq_along(dims)){       
+           #        row[[dims[j]]] <- tab[[i]][name,j]
+           #      }
+           #      
+           #      table$addRow(rowKey=name, values=row)
+           #    }
+           # 
+           #    }
+           
+   # D study (Composite table)------------
+         
+            if(isTRUE(self$options$comp)){
+               
+            table <- self$results$comp
+             
+            # Measures of D study---------------
+            gen<- as.vector(ds1$composite$generalizability)
+            depe<- as.vector(ds1$composite$dependability)
+            uni<- as.vector(ds1$composite$var.universe)
+            rel<- as.vector(ds1$composite$var.error.rel)
+            abs<- as.vector(ds1$composite$var.error.abs)
+            
+              row <- list()
+              
+              row[['generalizability']] <- gen
+              row[['dependability']] <- depe
+              row[['universe']] <- uni
+              row[['relative']] <- rel
+              row[['absolute']] <- abs
+              
+              table$setRow(rowNo = 1, values = row)
+              
+            } 
+   
+            if(isTRUE(self$options$bm)){
+              
+              table <- self$results$bm
+              
+              # measure---
+              gen<- ds1$between$generalizability
+              Generalizability<- diag(gen)
+              gen<- as.data.frame(Generalizability)
+              
+              dep<- ds1$between$dependability
+              Dependability<- diag(dep)
+              depe<- as.data.frame(Dependability)
+              
+              rel<- ds1$between$var.error.rel
+              Relative<- diag(rel)
+              rel<- as.data.frame(Relative)
+              
+              abs<- ds1$between$var.error.abs
+              Absolute<- diag(abs)
+              abs<- as.data.frame(Absolute)
+              
+              all<- data.frame(gen,depe,rel,abs)
+              #-----------
+              names<- dimnames(all)[[1]]
+              
+              for (name in names) {
+                
+                row <- list()
+                
+                row[['gen']] <- all[name,1]
+                row[['depe']] <- all[name,2]
+                row[['rel']] <- all[name,3]
+                row[['abs']] <- all[name,4]
+                
+                table$addRow(rowKey=name, values=row)
+                
+              }
+              
+              
+            }
+            
+            
+            
+            
+                      
+ }  
+             
+
+          
+          
+ 
+        })
+)
