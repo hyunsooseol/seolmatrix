4fb86a5bae2596333a143ff8dc951d8ff201067e
hyunsooseol <snow@cau.ac.kr>
2025-03-31 16:08:14 +0900

    fixed rmc

--- R/rmc.b.R
+++ R/rmc.b.R
@@ -1,5 +1,3 @@
-
-
 rmcClass <- if (requireNamespace('jmvcore', quietly = TRUE))
   R6::R6Class(
     "rmcClass",
@@ -61,38 +59,37 @@ rmcClass <- if (requireNamespace('jmvcore', quietly = TRUE))
         data[[covs]] <- jmvcore::toNumeric(data[[covs]])
         data <- na.omit(data)
         
-        if (isTRUE(self$options$rc) | isTRUE(self$options$plot)) {
+        if (isTRUE(self$options$rc)) {  
           # Repeated correlation-----------------------------------------------------
-          
+          set.seed(1234)
           res <- rmcorr::rmcorr(id, data[[dep]], data[[covs]], data)
           
           #-----------------------------------------
           
-          if (isTRUE(self$options$rc)) {
-            table <- self$results$rc
-            r <-  res$r
-            df <- res$df
-            p <- res$p
-            lower <-  res$CI[[1]]
-            upper <-   res$CI[[2]]
-            
-            row <- list()
-            row[['r']] <- r
-            row[['df']] <- df
-            row[['p']] <- p
-            row[['lower']] <- lower
-            row[['upper']] <- upper
-            table$setRow(rowNo = 1, values = row)
-          }
-          image <- self$results$plot
-          image$setState(res)
+          
+          table <- self$results$rc
+          r <-  res$r
+          df <- res$df
+          p <- res$p
+          lower <-  res$CI[[1]]
+          upper <-   res$CI[[2]]
+          
+          row <- list()
+          row[['r']] <- r
+          row[['df']] <- df
+          row[['p']] <- p
+          row[['lower']] <- lower
+          row[['upper']] <- upper
+          table$setRow(rowNo = 1, values = row)
         }
+        
         
         # Cross correlation-----------------------
         
-        if (isTRUE(self$options$cc) | isTRUE(self$options$plot1)) {
+        if (isTRUE(self$options$cc)){ 
           Measure1 <- as.vector(data[[dep]])
           Measure2 <- as.vector(data[[covs]])
+          set.seed(1234)
           r <- stats::ccf(Measure1, Measure2, plot = FALSE)
           
           #self$results$text$setContent(res1)
@@ -109,17 +106,31 @@ rmcClass <- if (requireNamespace('jmvcore', quietly = TRUE))
             row[['value']] <- res1[i, 2]
             table$addRow(rowKey = i, values = row)
           }
-          image1 <- self$results$plot1
-          image1$setState(r)
+          # image1 <- self$results$plot1
+          # image1$setState(r)
         }
       },
       
       .plot = function(image, ...) {
-        if (is.null(image$state))
+        
+        if (!self$options$plot)
           return(FALSE)
-        rc <- image$state
+        
+        data <- self$data
+        id <- self$options$id
+        dep <- self$options$dep
+        covs <- self$options$covs
+        # convert to appropriate data types
+        data[[dep]] <- jmvcore::toNumeric(data[[dep]])
+        data[[covs]] <- jmvcore::toNumeric(data[[covs]])
+        data <- na.omit(data)
+        
+        # Repeated correlation-----------------------------------------------------
+        set.seed(1234)
+        res <- rmcorr::rmcorr(id, data[[dep]], data[[covs]], data)
+        
         plot <- plot(
-          rc,
+          res,
           overall = FALSE,
           lty = 1,
           lwd = 3,
@@ -130,11 +141,24 @@ rmcClass <- if (requireNamespace('jmvcore', quietly = TRUE))
         TRUE
       },
       
+      
       .plot1 = function(image1, ...) {
-        if (is.null(image1$state))
-          return(FALSE)
-        res11 <- image1$state
-        plot1 <- plot(res11)
+        
+        data <- self$data
+        id <- self$options$id
+        dep <- self$options$dep
+        covs <- self$options$covs
+        # convert to appropriate data types
+        data[[dep]] <- jmvcore::toNumeric(data[[dep]])
+        data[[covs]] <- jmvcore::toNumeric(data[[covs]])
+        data <- na.omit(data)
+        
+        Measure1 <- as.vector(data[[dep]])
+        Measure2 <- as.vector(data[[covs]])
+        
+        set.seed(1234)
+        plot1 <- stats::ccf(Measure1, Measure2)
+        
         print(plot1)
         TRUE
       }
